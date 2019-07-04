cf334087d5dc27c0cae6ca1e8f492471d6aca579
Damian Brady <info@damianbrady.com.au>
2019-07-04 14:50:41 +1000

    Updated with breaking tests

--- app/welcometime.js
+++ app/welcometime.js
@@ -1,3 +1,16 @@
-exports.getCurrentTime = function() {
+exports.getCurrentTime = function () {
     return Date.now();
+}
+
+exports.addMonth = function (dt) {
+    return new Date(dt.setMonth(dt.getMonth() + 1));
+    /*
+    var d = new Date(dt.setMonth(dt.getMonth() + 1));
+    if (dt.getYear() == d.getYear() && dt.getMonth() + 2 == d.getMonth()) {
+        // we skipped a month, let's get the end of the next month
+        d = new Date(d.setMonth(d.getMonth()));
+        d = new Date(d.setDate(1)-1);
+    }
+    return d;
+    */
 }
--- test/welcomepagetest.js
+++ test/welcomepagetest.js
@@ -1,5 +1,7 @@
-var expect = require("chai").expect;
+var chai = require("chai");
+var chaidatetime = require("chai-datetime");
 var welcometime = require("../app/welcometime");
+chai.use(chaidatetime);
 
 describe("Get Time", function () {
     describe("Get welcome time", function () {
@@ -9,11 +11,28 @@ describe("Get Time", function () {
                 var retrievedTime = welcometime.getCurrentTime();
                 setTimeout(function () {
                     var laterTime = Date.now();
-                    expect(retrievedTime).to.be.greaterThan(startTime);
-                    expect(retrievedTime).to.be.lessThan(laterTime);
+                    chai.expect(retrievedTime).to.be.greaterThan(startTime);
+                    chai.expect(retrievedTime).to.be.lessThan(laterTime);
                     done();
                 }, 100);
             }, 100);
+        });
+    });
+    describe("Get next month", function() {
+        it("correctly adds a month to a basic date", function() {
+            var thisDate = new Date(2018,2,1);
+            var expectedDate = new Date(2018,3,1);
+            var oneMonth = welcometime.addMonth(thisDate);
+
+            chai.expect(oneMonth).to.equalDate(expectedDate);
+        });
+        
+        it("correctly adds a month to a tricky date", function() {
+            var thisDate = new Date(2018,0,30);
+            var expectedDate = new Date(2018,1,28);
+            var oneMonth = welcometime.addMonth(thisDate);
+
+            chai.expect(oneMonth).to.equalDate(expectedDate);
         });
     });
 });
